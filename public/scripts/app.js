a3754a583d12f4dc961eaa54f410ef6b04dcdbf1
Rachel Selkirk <rachel.selkirk@gmail.com>
2017-04-20 00:52:50 +0000

    added compose button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,68 +5,75 @@
  */
 
 
-
 $(document).ready(() => {
   function sanitize(str) {
     // TODO
     return str;
   }
 
-  
-
   function createTweetElement(tweetData) {
     return $(`
-          <article class='one-tweet tweet'>
-            <header>
-              <img class="avatar" src=${tweetData.user.avatars.small}>
-              <h2>${sanitize(tweetData.user.name)}</h2>
-              <p class="handle"> ${sanitize(tweetData.user.handle)} </p>
-            </header>
-            <div class="tweetBody">
-              <span class="tweet"> ${sanitize(tweetData.content.text)} </span>
-            </div>
-            <footer>
-              <span class="ageCounter">${sanitize(moment(tweetData.created_at).fromNow())}</span>
-            </footer>
-          </article>
-      `);
+      <article class='one-tweet tweet'>
+        <header>
+          <img class="avatar" src=${tweetData.user.avatars.small}>
+          <h2>${sanitize(tweetData.user.name)}</h2>
+          <p class="handle"> ${sanitize(tweetData.user.handle)} </p>
+        </header>
+        <div class="tweetBody">
+          <span class="tweet"> ${sanitize(tweetData.content.text)} </span>
+        </div>
+        <footer>
+          <span class="ageCounter">${(moment(tweetData.created_at).fromNow())}</span>
+        </footer>
+      </article>
+    `);
   }
+
+  function renderTweets(tweets) {
+    for (const tweet of tweets) {
+      const x = createTweetElement(tweet);
+      $('.all-tweets').prepend(x);
+    }
+  }
+
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: renderTweets,
+    });
+  }
+
 
   $('form').on('submit', function (event) {
     event.preventDefault();
-    if (($('textarea').val()).length = 140) {
+    if (($('textarea').val()).length === 0) {
       alert('Type something to make a tweet!');
       return;
     }
     if (($('textarea').val()).length > 140) {
       alert('Tweet must be 140 characters or fewer!');
       return;
-      }
+    }
     $.ajax({
       url: '/tweets',
       method: 'POST',
       data: $(this).serialize(),
-      success: loadTweets(),
+      success() {
+        loadTweets();
+        $('textarea').val('');
+      },
     });
   });
 
 
-  function loadTweets() {
-  $.ajax({
-      url: '/tweets',
-      method: 'GET',
-      success: renderTweets,
+$( ".compose" ).click(function() {
+  $( ".new-tweet" ).slideToggle( "slow", function() {
+  $('textarea').focus();
   });
-}
-
-loadTweets();
-
-function renderTweets(tweets) {
-    for (let tweet of tweets) {
-      let x = createTweetElement(tweet);
-      $('.all-tweets').prepend(x);
-    }
-  }
-
-
 });
+
+  $('.new-tweet').hide();
+  loadTweets();
+}); // doc.ready close.
+
